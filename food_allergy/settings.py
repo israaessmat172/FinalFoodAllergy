ccaba73eb4337f1d74fa02249cd8cafe6c8e99f3
israa <israa@gmail.com>
2023-05-12 03:15:10 +0300

    update req.txt

--- food_allergy/settings.py
+++ food_allergy/settings.py
@@ -91,12 +91,25 @@ WSGI_APPLICATION = "food_allergy.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
+        'NAME': 'FoodAllergiesDB',
+        'USER': 'israaessmat',
+        'PASSWORD': 'israa',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
