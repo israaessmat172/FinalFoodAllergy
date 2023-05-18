c61ca95554744b1a6c8a543e73b808f31fbe2f06
israa <israa@gmail.com>
2023-05-18 05:13:57 +0300

    add response

--- posts/serializers.py
+++ posts/serializers.py
@@ -20,13 +20,36 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+# class PostSerializer(serializers.ModelSerializer):
+#     owner = serializers.SlugRelatedField(slug_field='username',read_only=True)
+#     comments = CommentSerializer(many=True, read_only=True, source='comment_set')
+#     owner_profile_pic = serializers.ImageField(source='owner.profile_pic', read_only=True)
+#     allergy_arabic_name = serializers.CharField(source='allergy.arabicName', read_only=True)
+#     allergy_english_name = serializers.CharField(source='allergy.englishName', read_only=True)
+#     is_liked = serializers.BooleanField(read_only=True)
+#     class Meta:
+#         model = Post
+#         # fields = ['id','owner','comments','is_liked','title','image','created_at','updated_at']
+#         fields = ['id','owner','comments','is_liked','title','image','created_at','updated_at','owner_profile_pic',
+#                   "allergy_arabic_name","allergy_english_name", "likes"]
+
+#         extra_kwargs = { 'likes':{'read_only':True}}
+
+
+#     def get_is_liked(self, obj):
+#         request = self.context.get('request')
+#         if request and request.user.is_authenticated:
+#             if request.user in obj.likes.all():
+#                 return True
+#         return False
+
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.SlugRelatedField(slug_field='username',read_only=True)
     comments = CommentSerializer(many=True, read_only=True, source='comment_set')
     owner_profile_pic = serializers.ImageField(source='owner.profile_pic', read_only=True)
     allergy_arabic_name = serializers.CharField(source='allergy.arabicName', read_only=True)
     allergy_english_name = serializers.CharField(source='allergy.englishName', read_only=True)
-    is_liked = serializers.BooleanField(read_only=True)
+    is_liked = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Post
         # fields = ['id','owner','comments','is_liked','title','image','created_at','updated_at']
