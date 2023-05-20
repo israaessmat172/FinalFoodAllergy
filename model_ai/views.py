9bfd9bdbf026665c7528c127c1d662539f0570b3
israa <israa@gmail.com>
2023-05-20 16:59:40 +0300

    edit def ai

--- model_ai/views.py
+++ model_ai/views.py
@@ -16,6 +16,8 @@ from tensorflow.keras.preprocessing import image
 from rest_framework.permissions import IsAuthenticated
 import io
 from django.shortcuts import get_object_or_404
+from django.http import Http404
+
 # from rest_framework import permissions
 
 class PredictAPIView(APIView):
@@ -29,16 +31,16 @@ class PredictAPIView(APIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             img = serializer.validated_data['image']
-            img = img.read()  # extract the binary data from the InMemoryUploadedFile
+            img = img.read()  
             print("Image read from InMemoryUploadedFile.")
-            img = tf.image.decode_image(img, channels=3)  # decode the image
+            img = tf.image.decode_image(img, channels=3)  
             print("Image decoded.")
-            img = tf.image.resize_with_pad(img, 224, 224)  # resize the image to match the input shape of the model
+            img = tf.image.resize_with_pad(img, 224, 224) 
             print("Image resized with padding.")
-            img = np.array(img) / 255.0  # normalize the pixel values to be between 0 and 1
+            img = np.array(img) / 255.0  
             print("Image pixel values normalized.")
 
-            # load the model and get the prediction
+       
             print("Loading model...")
             model = tf.keras.models.load_model('model_ai/model.h5', custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
             model.build((None, 224, 224, 3))
@@ -47,11 +49,8 @@ class PredictAPIView(APIView):
             print("Prediction :", prediction)
             prediction_class = np.argmax(prediction)
             print("prediction_classsssssss:", prediction_class)
-
             predicted_class_index = np.argmax(prediction)
-            #####  WWWWHHHHHHATTT TTTHHHEEEEE ***** (:---:)
-            # data_dir = pathlib.Path('/content/drive/MyDrive/New folder/train')
-            # class_names = np.array(sorted([item.name for item in data_dir.glob('*')]))
+         
             class_names = ['Burger', 'Dairy product', 'Donut', 'Egg', 'Meat', 'Noodles-Pasta', 'Pizza',
             'Sandwich', 'Seafood', 'cake', 'hotDog','sushi']   
 
@@ -59,38 +58,100 @@ class PredictAPIView(APIView):
             print("Prediction class:", prediction_class)
             print("Prediction NAAAAMAMMMMMMMEEEEEEEEEEE:", predicted_class_name)
 
-            # look up the category in the database and serialize the response
-            # try:
-            #     category = Category.objects.get(pk=prediction_class)
-            #     print("Category found:", category)
-            #     serializer = CategorySerializer(category)
-
+           
             try:
                 category = get_object_or_404(Category, pk=prediction_class)
-                print("Category found:", category)
-                serializer = CategorySerializer(category)
-
-
-                # get the food and allergy for the category
+                
+         
                 food = category.food_set.first()
-                food_name = food.englishName
-                print("Food found:", food)
-
                 allergies = food.allergy_set.all()
                 allergy_names = [allergy.englishName for allergy in allergies]
-                print("Allergies found:", allergy_names)
+                food_name = food.englishName
 
-                # add food name and allergy names to the response
-                data = serializer.data
-                data['allergies'] = allergy_names
-                data['food_name'] = food_name
-
-                # return the response   
+               
+                data = {
+                    'category_name': category.name,
+                    'food_name': food_name,
+                    'allergies': allergy_names,
+                }
+                
+               
                 print("Returning the response...")
                 return Response(data, status=status.HTTP_200_OK)
-            except Category.DoesNotExist:
+            
+            except Http404:
                 print("Category not found.")
                 return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+    # def post(self, request):
+    #     serializer = self.serializer_class(data=request.data)
+    #     if serializer.is_valid():
+    #         img = serializer.validated_data['image']
+    #         img = img.read()  # extract the binary data from the InMemoryUploadedFile
+    #         print("Image read from InMemoryUploadedFile.")
+    #         img = tf.image.decode_image(img, channels=3)  # decode the image
+    #         print("Image decoded.")
+    #         img = tf.image.resize_with_pad(img, 224, 224)  # resize the image to match the input shape of the model
+    #         print("Image resized with padding.")
+    #         img = np.array(img) / 255.0  # normalize the pixel values to be between 0 and 1
+    #         print("Image pixel values normalized.")
+
+    #         # load the model and get the prediction
+    #         print("Loading model...")
+    #         model = tf.keras.models.load_model('model_ai/model.h5', custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
+    #         model.build((None, 224, 224, 3))
+    #         print("Model loaded.")
+    #         prediction = model.predict(np.array([img]))
+    #         print("Prediction :", prediction)
+    #         prediction_class = np.argmax(prediction)
+    #         print("prediction_classsssssss:", prediction_class)
+
+    #         predicted_class_index = np.argmax(prediction)
+    #         #####  WWWWHHHHHHATTT TTTHHHEEEEE ***** (:---:)
+    #         # data_dir = pathlib.Path('/content/drive/MyDrive/New folder/train')
+    #         # class_names = np.array(sorted([item.name for item in data_dir.glob('*')]))
+    #         class_names = ['Burger', 'Dairy product', 'Donut', 'Egg', 'Meat', 'Noodles-Pasta', 'Pizza',
+    #         'Sandwich', 'Seafood', 'cake', 'hotDog','sushi']   
+
+    #         predicted_class_name = class_names[predicted_class_index]
+    #         print("Prediction class:", prediction_class)
+    #         print("Prediction NAAAAMAMMMMMMMEEEEEEEEEEE:", predicted_class_name)
+
+    #         # look up the category in the database and serialize the response
+    #         # try:
+    #         #     category = Category.objects.get(pk=prediction_class)
+    #         #     print("Category found:", category)
+    #         #     serializer = CategorySerializer(category)
+
+    #         try:
+    #             category = get_object_or_404(Category, pk=prediction_class)
+    #             print("Category found:", category)
+    #             serializer = CategorySerializer(category)
+
+
+    #             # get the food and allergy for the category
+    #             food = category.food_set.first()
+    #             food_name = food.englishName
+    #             print("Food found:", food)
+
+    #             allergies = food.allergy_set.all()
+    #             allergy_names = [allergy.englishName for allergy in allergies]
+    #             print("Allergies found:", allergy_names)
+
+    #             # add food name and allergy names to the response
+    #             data = serializer.data
+    #             data['allergies'] = allergy_names
+    #             data['food_name'] = food_name
+
+    #             # return the response   
+    #             print("Returning the response...")
+    #             return Response(data, status=status.HTTP_200_OK)
+    #         except Category.DoesNotExist:
+    #             print("Category not found.")
+    #             return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
 
 
 # class PredictAPIView(APIView):
@@ -99,6 +160,8 @@ class PredictAPIView(APIView):
 #     permission_classes = [IsAuthenticated]
       
 #     serializer_class = PredictSerializer
+
+
 
 #     def post(self, request):
 #         serializer = self.serializer_class(data=request.data)
